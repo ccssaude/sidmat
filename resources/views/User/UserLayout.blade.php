15357bd49917568e2068ad6f3b7fc56a3ac2dd97
Denilson <denilsonmucavel@outlook.com>
2023-06-12 18:12:11 +0200

    Setting section "Yield"

--- resources/views/User/UserLayout.blade.php
+++ resources/views/User/UserLayout.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>SIDMAT | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -180,8 +180,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{asset('dist/img/AdminLTELogo.png" alt="AdminLTE Logo')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light"><b>SIDMAT</b></span>
     </a>
 
     <!-- Sidebar -->
@@ -851,10 +851,10 @@
     <!-- /.sidebar -->
   </aside>
 
-
+  @yield('content')
 
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2023 <a href="">ccsaude.org.mz</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
